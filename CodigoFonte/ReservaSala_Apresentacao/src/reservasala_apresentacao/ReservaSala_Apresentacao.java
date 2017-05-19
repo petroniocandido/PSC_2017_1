1e3a16f5d91bf3c9300380ce7abad08bba8cd6c6
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2017-05-19 16:05:40 -0300

    UC2.1: DAO's - #1

--- CodigoFonte/ReservaSala_Apresentacao/src/reservasala_apresentacao/ReservaSala_Apresentacao.java
+++ CodigoFonte/ReservaSala_Apresentacao/src/reservasala_apresentacao/ReservaSala_Apresentacao.java
@@ -7,6 +7,10 @@ package reservasala_apresentacao;
 
 import br.edu.ifnmg.PSC.ReservaSala.Aplicacao.Sala;
 import br.edu.ifnmg.PSC.ReservaSala.Aplicacao.ViolacaoRegraNegocioException;
+import br.edu.ifnmg.PSC.ReservaSala.Persistencia.DAOGenerico;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,18 +22,14 @@ public class ReservaSala_Apresentacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Sala s = new Sala();
-        
+       
         try {
-        
-            s.setNome("1234");
-            
-            s.setCapacidade(5);
-            
-        } catch(ViolacaoRegraNegocioException ex){
-            System.out.println(ex.getMessage());
+            DAOGenerico<Sala> teste = new DAOGenerico<>();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReservaSala_Apresentacao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReservaSala_Apresentacao.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
     
 }
