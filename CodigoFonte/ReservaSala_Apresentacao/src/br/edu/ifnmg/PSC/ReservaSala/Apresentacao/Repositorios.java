607f982043347e4e51b5241e9db0385ddd9d32ba
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2017-06-09 14:26:56 -0300

    UC 2.1 #2 - Camada de apresentação

---
+++ CodigoFonte/ReservaSala_Apresentacao/src/br/edu/ifnmg/PSC/ReservaSala/Apresentacao/Repositorios.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.PSC.ReservaSala.Apresentacao;
+
+import br.edu.ifnmg.PSC.ReservaSala.Aplicacao.SalaRepositorio;
+import br.edu.ifnmg.PSC.ReservaSala.Persistencia.SalaDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author petronio
+ */
+public class Repositorios {
+    
+    static SalaRepositorio salaDAO = null;
+    
+    public static SalaRepositorio getSalaRepositorio(){
+        if(salaDAO == null)
+            try {
+                salaDAO = new SalaDAO();
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Repositorios.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(Repositorios.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        return salaDAO;
+    }
+    
+}
